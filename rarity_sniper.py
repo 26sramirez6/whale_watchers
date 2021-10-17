dec34fe19a8953222944cb772a9a045ff7877920
Saul Ramirez <26sramirez6@gmail.com>
2021-10-17 15:09:46 -0500

    organizing a little

--- rarity_sniper.py
+++ rarity_sniper.py
@@ -17,7 +17,7 @@ from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 import datetime
 from lxml.html import fromstring
-from multiprocessing import Pool, Process, Manager
+from multiprocessing import Pool, Process, Manager, Lock
 import pickle
 from web3 import Web3
 import urllib3
@@ -28,159 +28,33 @@ OS_URL = "https://api.opensea.io/api/v1/events"
 ETH_URL = "https://api.etherscan.io/api"
 QRY_STR = {"only_opensea":"false","offset":"0","limit":"20", "event_type":"successful"}
 HEADERS = {"Accept": "application/json"}
-CONTRACT = "0xf7143ba42d40eaeb49b88dac0067e54af042e963" #"0xf7143ba42d40eaeb49b88dac0067e54af042e963" #"0x9A95eCEe5161b888fFE9Abd3D920c5D38e8539dA"
+CONTRACT = "0x219b8ab790decc32444a6600971c7c3718252539" #"0xf7143ba42d40eaeb49b88dac0067e54af042e963" #"0xf7143ba42d40eaeb49b88dac0067e54af042e963" #"0x9A95eCEe5161b888fFE9Abd3D920c5D38e8539dA"
 WEB3_URL = "https://mainnet.infura.io/v3" #"wss://mainnet.infura.io/ws/v3"
 WEB3_API_KEY = "76e9e5d5de124620a24a3430699db0c3"
 SCRAPER_API_KEY = "40b2cc4259dffdd12466035827b4c3e5"
 SCRAPER_API_BASE = "http://api.scraperapi.com"
-
-HEADERS = [
-    # Firefox 77 Mac
-    OrderedDict([
-        ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0"),
-        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"),
-        ("Accept-Language", "en-US,en;q=0.5"),
-        ("Referer", "https://www.google.com/"),
-        ("DNT", "1"),
-        ("Connection", "keep-alive"),
-        ("Upgrade-Insecure-Requests", "1")
-    ]),
-    # Firefox 77 Windows
-    OrderedDict([
-        ("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0"),
-        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"),
-        ("Accept-Language", "en-US,en;q=0.5"),
-        ("Accept-Encoding", "gzip, deflate, br"),
-        ("Referer", "https://www.google.com/"),
-        ("DNT", "1"),
-        ("Connection", "keep-alive"),
-        ("Upgrade-Insecure-Requests", "1")
-    ]),
-    # Chrome 83 Mac
-    OrderedDict([
-        ("Connection", "keep-alive"),
-        ("DNT", "1"),
-        ("Upgrade-Insecure-Requests", "1"),
-        ("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"),
-        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"),
-        ("Sec-Fetch-Site", "none"),
-        ("Sec-Fetch-Mode", "navigate"),
-        ("Sec-Fetch-Dest", "document"),
-        ("Referer", "https://www.google.com/"),
-        ("Accept-Encoding", "gzip, deflate, br"),
-        ("Accept-Language", "en-GB,en-US;q=0.9,en;q=0.8")
-    ]),
-    # Chrome 83 Windows
-    OrderedDict([
-        ("Connection", "keep-alive"),
-        ("Upgrade-Insecure-Requests", "1"),
-        ("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36"),
-        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"),
-        ("Sec-Fetch-Site", "same-origin"),
-        ("Sec-Fetch-Mode", "navigate"),
-        ("Sec-Fetch-User", "?1"),
-        ("Sec-Fetch-Dest", "document"),
-        ("Referer", "https://www.google.com/"),
-        ("Accept-Encoding", "gzip, deflate, br"),
-        ("Accept-Language", "en-US,en;q=0.9")
-    ])
-]
-
-def get_header():
-    return np.random.choice(HEADERS)
+SCRAPER_API_PROXY_POOL = "http://scraperapi.{}:{}@proxy-server.scraperapi.com:8001"
 
 
-class ProxyRotator:
-    @staticmethod
-    def parse_sslproxies():
-        response = requests.get("https://sslproxies.org/") 
-        soup = BeautifulSoup(response.content, 'html5lib')
-        proxies = list(
-            map(lambda x:x[0]+':'+x[1], list(zip(map(lambda x:x.text, soup.findAll('td')[::8]), 
-                                                  map(lambda x:x.text, soup.findAll('td')[1::8])))))            
-        return [None] if len(proxies)==0 else proxies
-   
-    @staticmethod
-    def parse_freeproxy():
-        response = requests.get("https://free-proxy-list.net", headers=get_header())
-        parser = fromstring(response.text)
-        proxies = []
-        for i in parser.xpath('//tbody/tr')[:100]:
-            if i.xpath('.//td[7][contains(text(),"yes")]'):
-                proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
-                proxies.append(proxy)
-        return [None] if len(proxies)==0 else proxies
+def request_through_proxy_pool(url):
+    params = {"country_code": "us", "premium":"true"}
+    params_str = ".".join(["{}={}".format(k,v) for k, v in params.items()])
+    proxies = {"http": SCRAPER_API_PROXY_POOL.format(params_str, SCRAPER_API_KEY)}
+    response = requests.request("GET", url, proxies=proxies, verify=False)
+    return response
+
+def request_direct(url):
+    params = {"api_key": SCRAPER_API_KEY, "url": url, "premium":"true"}
+    response = requests.request("GET", url, params=params, verify=False)
+    return response
+
+class SharedState:
+    def __init__(self):
+        self.trait_archives = {}
+        self.metadatas = {}
         
-    @staticmethod
-    def none():
-        return [None]
-   
-    PROVIDERS = {
-        None : none,
-        "https://www.sslproxies.org/": parse_sslproxies,
-        "https://free-proxy-list.net/": parse_freeproxy}
 
-    def __init__(self, p={None:.06, "https://www.sslproxies.org/":.47, "https://free-proxy-list.net/":.47}):
-        self.tables_by_provider = {}
-        self.full_tables = []
-        self.consecutive_failures_by_provider = {}
-        self.consecutive_failures_by_proxy = {k: {} for k in ProxyRotator.PROVIDERS}
-        self.full_consecutive_failures = {}
-        self.total_failures_by_provider = {}
-        self.total_attempts_by_provider = {}
-        self.total_attempts_by_proxy = {}
-        self.total_failures_by_proxy = {}
-        self.last_proxy = None
-        self.last_provider = None
-        self.last_success = True
-        for k,v in ProxyRotator.PROVIDERS.items():
-            provider_proxies = v.__func__()                
-            self.tables_by_provider[k] = provider_proxies
-            self.full_tables.extend(provider_proxies)
-            self.consecutive_failures_by_provider[k] = 0
-            self.total_attempts_by_provider[k] = 0
-            self.total_failures_by_provider[k] = 0
-            for proxy in provider_proxies:
-                self.consecutive_failures_by_proxy[proxy] = 0
-                self.total_attempts_by_proxy[proxy] = 0
-                self.total_failures_by_proxy[proxy] = 0
-            
-        self.providers = list(self.tables_by_provider)
-        self.probabilities = [p[provider] for provider in self.providers]
-        
-    def get_proxy(self):
-        if self.last_success:
-            return self.last_proxy, self.last_provider
-        
-        provider = np.random.choice(self.providers, p=self.probabilities)
-        proxy = np.random.choice(self.tables_by_provider[provider])
-        return proxy, provider
-    
-    def register_results(self, proxy, provider, success):
-        if success:
-            self.consecutive_failures_by_provider[provider] = 0
-            self.consecutive_failures_by_proxy[proxy] = 0
-        else:
-            self.consecutive_failures_by_provider[provider] += 1
-            self.consecutive_failures_by_proxy[proxy] += 1
-            self.total_failures_by_provider[provider] += 1
-            self.total_failures_by_proxy[proxy] += 1
-            
-        self.total_attempts_by_provider[provider] += 1
-        self.total_attempts_by_proxy[proxy] += 1
-        
-        self.last_proxy = proxy
-        self.last_provider = provider
-        self.last_success = success
-
-    
-    def print_results(self):
-        print("*********ROTATOR RESULTS*********")
-        for k,v in self.total_attempts_by_provider.items():
-            print(k, "success rate", "0 attempts" if v==0 else (v - self.total_failures_by_provider[k]) / v)
-
-
-def scan_batch_ipfs(tid, indices, base_uri):
+def scan_batch(tid, mutex, shared, indices, base_uri, token_idx_format):
     trait_archives, metadatas = {}, {}
     failures = 0
     remaining = set(indices)
@@ -194,23 +68,22 @@ def scan_batch_ipfs(tid, indices, base_uri):
         remaining.difference_update(processed)
         print("major attempt {}. Remaining: {}".format(major_attempt, len(remaining)))
         for index in remaining:
-            url = "{}/{}".format(base_uri, index)
+            url = "{}/{}".format(base_uri, token_idx_format.format(index))
                 
             try:
-                params = {"api_key": SCRAPER_API_KEY, "url": url, "premium":"true"}
-                proxies = {"http": "http://scraperapi.country_code=us:{}@proxy-server.scraperapi.com:8001".format(SCRAPER_API_KEY)}
-                response = requests.request("GET", url, proxies=proxies, verify=False)
-                
-                loads = json.loads(response.text)
-                
+                response = request_through_proxy_pool(url)
+                loads = json.loads(response.text)                
                 traits = loads['attributes']
                 name = loads['name']
             except Exception as e:
                 print("FAILED on index {}, response code {}, error {}, host {}".format(index, response.status_code if "response" in locals() else "--", e, url))
                 failures += 1
                 continue
-                
             
+            # for when the mess up metadata
+            if isinstance(name, list):
+                name = name[0]
+                
             metadatas[name] = traits
             for i, trait in enumerate(traits):
                 trait_type = trait['trait_type']
@@ -223,30 +96,69 @@ def scan_batch_ipfs(tid, indices, base_uri):
                 trait_values_dict[trait_value] += 1
             print("processed index", index)
             processed.add(index)
-    pickle.dump(
-        trait_archives,
-        open('trait_archives{}.pickle'.format(tid), 'wb'),
-        protocol=pickle.HIGHEST_PROTOCOL)
-    pickle.dump(
-        metadatas,
-        open('metadatas{}.pickle'.format(tid), 'wb'),
-        protocol=pickle.HIGHEST_PROTOCOL)
+    mutex.acquire()
+    try:
+        for k,v in trait_archives.items():
+            if k not in shared.trait_archives:
+                shared.trait_archives[k] = {}
+            trait_archive = shared.trait_archives[k]
+            for name, count in v.items():
+                if name not in trait_archive:
+                    trait_archive[name] = 0
+                trait_archive[name] += count
+                
+        shared.metadatas.update(metadatas)
+    finally:
+        mutex.release()
+        
+#     pickle.dump(
+#         trait_archives,
+#         open('trait_archives{}.pickle'.format(tid), 'wb'),
+#         protocol=pickle.HIGHEST_PROTOCOL)
+#     pickle.dump(
+#         metadatas,
+#         open('metadatas{}.pickle'.format(tid), 'wb'),
+#         protocol=pickle.HIGHEST_PROTOCOL)
+
     print("batch completed, successes: {}, failures: {}".format(len(indices)-failures, failures))
 
-def run_batches(base_uri, collection_size, process_count):
+def generate_metadatas(base_uri, token_idx_format, collection_size, process_count):
     processes = []
     batch_size = collection_size / process_count
+    shared = SharedState()
+    mutex = Lock()
     for j in range(process_count):
         start = 1 + int(j*(batch_size))
         end = collection_size if j==process_count-1 else int((j+1)*(batch_size))
-        p = Process(target=scan_batch_ipfs, args=(j, range(start, end), base_uri))
+        p = Process(target=scan_batch, args=(j, mutex, shared, range(start, end), base_uri, token_idx_format))
         processes.append(p)
       
     for p in processes:
         p.start()
     for p in processes:
         p.join()
-
+        
+    # identify unique traits, correct the metadatas by adding missing global categories
+    trait_categories = shared.trait_archives.keys()
+    for trait, options in shared.trait_archives.items():
+        trait_presence_count = sum(options.values())
+        missing = collection_size - trait_presence_count
+        options["None"] = missing
+    for token, attributes in shared.metadatas.items():
+        token_attributes = set([attr["trait_type"] for attr in attributes])
+        for category in trait_categories:
+            if category not in token_attributes:
+                attributes.append({"trait_type" : category, "value": "None"})
+    
+    pickle.dump(
+        shared.trait_archives,
+        open('trait_archives.pickle', 'wb'),
+        protocol=pickle.HIGHEST_PROTOCOL)
+    pickle.dump(
+        shared.metadatas,
+        open('metadatas.pickle', 'wb'),
+        protocol=pickle.HIGHEST_PROTOCOL)
+    
     print("*******COMPLETED METADATA SCAN*******")
     
 
@@ -262,14 +174,13 @@ def get_contract_abi(contract_address, etherscan_api_key):
     return loads["result"]
         
 def url_from_ipfs(ipfs_address):
-
     if ( "http" in ipfs_address ):
         return ipfs_address
 
     IPFS_GATEWAY = np.random.choice(
         [
 #         "https://cloudflare-ipfs.com/ipfs/",
-        "https://ipfs.io/ipfs/",
+        "http://ipfs.io/ipfs/",
 #         "https://ipfs.infura.io:5001/api/v0/cat?arg=",
 #         "https://ipfs.tubby.cloud/ipfs/",
 #         "https://ravencoinipfs-gateway.com/ipfs/",
@@ -282,7 +193,6 @@ def url_from_ipfs(ipfs_address):
     )
 
     url = IPFS_GATEWAY + ipfs_address.strip("ipfs://")
-    
     return url
 
 def get_token_uri(contract_address, token_id, abi): 
@@ -295,76 +205,30 @@ def get_token_uri(contract_address, token_id, abi):
     
     base_uri = "/".join(token_uri.split("/")[:-1])
     
-    return base_uri, token_uri
+    split = token_uri.split("/")
+    base_uri = "/".join(split[:-1])
+    token_idx_format = split[-1].replace(str(token_id), "{}")
+    return base_uri, token_uri, token_idx_format
 
 
 def spin_until_reveal(contract_abi):
-    main_rotator = ProxyRotator()
     is_live = False
     while not is_live:
-        proxy, provider = main_rotator.get_proxy()
-        base_uri, token_uri = get_token_uri(CONTRACT, np.random.randint(1,20), contract_abi)
-        if proxy is None:
-            proxies = {}
-        else:
-            proxies = {"http":proxy}
+        base_uri, token_uri, token_idx_format = get_token_uri(CONTRACT, np.random.randint(1,20), contract_abi)
         try:
-            response = requests.request(
-                    "GET",
-                    token_uri,
-                    headers=get_header(), 
-                    timeout=1,
-                    proxies=proxies)
-            
+            response = request_direct(token_uri)
             loads = json.loads(response.text)
             traits = loads['attributes']
             name = loads['name']
             is_live = True
         except Exception:
             print("{}: reveal not live, sleeping".format(datetime.datetime.now()))
-            main_rotator.register_results(proxy, provider, False)
             time.sleep(5)
             
     print("{} REVEAL LIVE on {}, BEGINNING SCAN".format(datetime.datetime.now(), token_uri))
-    return base_uri, token_uri
-    
-if __name__ == "__main__":
-    
-    collection_size = 9999
-    start = datetime.datetime.now()
-    contract_abi = get_contract_abi(CONTRACT, ETHERSCAN_API_KEY)
-    base_uri, token_uri = spin_until_reveal(contract_abi)
-    process_count = 50
-    run_batches(base_uri, collection_size, process_count)
-    
-    trait_archives = {}
-    metadatas = {}
-    for i in range(process_count):
-        thread_traits_archive = pickle.load(open('trait_archives{}.pickle'.format(i), 'rb'))
-        thread_metadatas = pickle.load(open('metadatas{}.pickle'.format(i), 'rb'))
-        
-        metadatas.update(thread_metadatas)
-        for k,v in thread_traits_archive.items():
-            if k not in trait_archives:
-                trait_archives[k] = {}
-            trait_archive = trait_archives[k]
-            for name, count in v.items():
-                if name not in trait_archive:
-                    trait_archive[name] = 0
-                trait_archive[name] += count
-    
-    # identify unique traits, correct the metadatas by adding missing global categories
-    trait_categories = trait_archives.keys()
-    for trait, options in trait_archives.items():
-        trait_presence_count = sum(options.values())
-        missing = collection_size - trait_presence_count
-        options["None"] = missing
-    for token, attributes in metadatas.items():
-        token_attributes = set([attr["trait_type"] for attr in attributes])
-        for category in trait_categories:
-            if category not in token_attributes:
-                attributes.append({"trait_type" : category, "value": "None"})
-    
+    return base_uri, token_uri, token_idx_format
+
+def generate_rankings():
     specific_trait_rarities = {}
     average_trait_rarities = {}
     for trait, options in trait_archives.items():
@@ -400,3 +264,19 @@ if __name__ == "__main__":
     with open("rankings.txt", "wb") as f:
         f.write("\n".join(rankings).encode("utf-8"))
     print("Runtime: {}".format((datetime.datetime.now() - start).total_seconds()))
+
+
+def map_against_os():
+    return
+    
+    
+if __name__ == "__main__":
+    collection_size = 8888
+    start = datetime.datetime.now()
+    contract_abi = get_contract_abi(CONTRACT, ETHERSCAN_API_KEY)
+    base_uri, token_uri, token_idx_format = spin_until_reveal(contract_abi)
+    process_count = 4
+    generate_metadatas(base_uri, token_idx_format, collection_size, process_count)
+    generate_rankings()
+    
+    
